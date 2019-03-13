782b67cd33f2b981230323f71156612e71f31235
Lucas Löffel <lucas@loeffel.io>
2019-03-13 14:56:21 +0100

    updated header asset

--- header.php
+++ header.php
@@ -4,6 +4,6 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Netro</title>
-    <link rel="stylesheet" type="text/css" href="<?= asset('dist/app.css') . '?v=' . rand(1, 9999) ?>">
+    <link rel="stylesheet" type="text/css" href="<?= asset('dist/app.css', true); ?>">
 </head>
 <body>
